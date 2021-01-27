b54531d49981d75615301179489d12cad691e4bd
JuanSuarezZ <jussuarezc@correo.udistrital.edu.co>
2021-01-27 17:23:47 -0500

    first

---
+++ pruebas.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+
+psycopg2.connect(
+    dbname="sql10394747",
+    user="sql10394747",
+    password="cMluk7p3z6",
+    host="sql10.freesqldatabase.com",
+    port="3306"
+    )
